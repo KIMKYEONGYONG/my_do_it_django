2dbce4aeedbff24f1fcfde5e37beff4e0b4a69f9
KIMKYEONGYONG <rlaruddyd1@naver.com>
2022-03-30 16:37:51 +0900

    카테고리 페이지

--- blog/tests.py
+++ blog/tests.py
@@ -112,3 +112,19 @@ class TestView(TestCase):
 
         self.assertIn(self.user_trump.username.upper(), main_area.text)
         self.assertIn(self.post_001.content, post_area.text)
+
+    def test_category_page(self):
+        responce = self.client.get(self.category_programming.get_absolute_url())
+        self.assertEqual(responce.status_code, 200)
+
+        soup = BeautifulSoup(responce.content, 'html.parser')
+        self.navbar_test(soup)
+        self.category_card_test(soup)
+
+        self.assertIn(self.category_programming.name, soup.h1.text)
+
+        main_area = soup.find('div', id='main-area')
+        self.assertIn(self.category_programming.name, main_area.text)
+        self.assertIn(self.post_001.title, main_area.text)
+        self.assertNotIn(self.post_002.title, main_area.text)
+        self.assertNotIn(self.post_003.title, main_area.text)
